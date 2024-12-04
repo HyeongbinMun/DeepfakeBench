fe62c249cb3b531b2099c063a220e98cae9cca5d
DESKTOP-5A81ABD\Jalee <hellojeongaelee@gmail.com>
2024-12-04 16:39:07 +0900

    modify npr/ucf inference code

--- training/infer_npr_class.py
+++ training/infer_npr_class.py
@@ -8,18 +8,16 @@ import torch
 import torch.nn.functional as F
 from PIL import Image
 from npr_networks.resnet import resnet50
-import npr_networks.resnet as resnet
 from torchvision import transforms
 from preprocess_infer import img_face_crop
 from PIL import Image
-from collections import OrderedDict
-from copy import deepcopy
+import torch.nn as nn
 
 
 class NPRInference:
     def __init__(self,
                  face_crop_predictor='/nfs_shared/deepfake/pretrained_model/shape_predictor_81_face_landmarks.dat',
-                 npr_model_path = '/nfs_shared/deepfake/pretrained_model/NPR.pth',
+                 npr_model_path = '/nfs_shared/deepfake/pretrained_model/npr_final.pth',
                  save_path='user/'):
         
         self.face_crop_predictor = face_crop_predictor
@@ -39,75 +37,60 @@ class NPRInference:
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.enabled = False
     
-    def load_single_image(self, image_path, opt):
-        transform = transforms.Compose([
-            transforms.Resize((opt['loadSize'], opt['loadSize'])),  # 이미지 리사이즈
-            transforms.CenterCrop(opt['cropSize']),  # 이미지 크롭
-            transforms.ToTensor(),  # PIL 이미지를 PyTorch 텐서로 변환
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # 정규화
-        ])
+    def interpolate(self, img, factor):
+        return F.interpolate(F.interpolate(img, scale_factor=factor, mode='nearest', recompute_scale_factor=True), scale_factor=1/factor, mode='nearest', recompute_scale_factor=True)
+
+
+    def predict_image(self, image_path, model):
+        trans = transforms.Compose([ transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) ])
+        img = trans(Image.open(image_path).convert('RGB')).unsqueeze(0)
+        _, c, w, h = img.shape
+        if w%2 == 1: img = img[:, :, :-1,:  ]
+        if h%2 == 1: img = img[:, :, :  ,:-1]
+        NPR = img - self.interpolate(img, 0.5)
+        with torch.no_grad():
+            x   = model.conv1(NPR*2.0/3.0)
+            x   = model.bn1(x)
+            x   = model.relu(x)
+            x   = model.maxpool(x)
+            x   = model.layer1(x)
+            x   = model.layer2(x).mean(dim=(2,3), keepdim=False)
+            x   = model.fc1(x)
         
-        image = Image.open(image_path).convert('RGB')  # 이미지를 RGB로 로드
-        image = transform(image)  # 변환 적용
-        image = image.unsqueeze(0)  # 배치 차원 추가 (batch_size, C, H, W)
-        
-        return image
-    
-    def npr_image(self, image_path):
-        image = cv2.cvtColor(image_path, cv2.COLOR_BGR2RGB)
-        image = torch.Tensor(image)
-        image = image.permute(2,0,1).contiguous()
-        gray_img = torch.mean(image, dim=0, keepdim=True)
+        pred = x.sigmoid().cpu().numpy()
+        return pred, NPR
 
-        reconstructed = F.interpolate(F.interpolate(gray_img, scale_factor=0.5, mode='nearest', recompute_scale_factor=True), scale_factor=2, mode='nearest', recompute_scale_factor=True)
-
-        npr  = image - reconstructed
-        return npr
-
-    
     def run_inference(self, image_path):
+
+        NPRmodel = resnet50()
+        NPRmodel.fc1 = nn.Linear(512, 1)
+        NPRmodel.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1, bias=False)
+        NPRmodel.load_state_dict(torch.load(self.model_path, map_location='cpu'), strict=True)
+        
+
+        pred, NPR = self.predict_image(image_path, NPRmodel)
 
         img = img_face_crop(image_path, self.face_crop_predictor)
         cropped_img_np = np.array(img)
 
-        model = resnet50(num_classes=1)
-        state_dict = torch.load(self.model_path, map_location='cpu')['model']
-        pretrained_dict = OrderedDict()
-        for ki in state_dict.keys():
-            pretrained_dict[ki[7:]] = deepcopy(state_dict[ki])
-        model.load_state_dict(pretrained_dict, strict=True)
-        model.cuda()
-        model.eval()
-        opt = {
-        'loadSize': 256,  # 이미지 로드 크기
-        'cropSize': 224,  # 크롭 크기
-        }
-
-        image_tensor = self.load_single_image(image_path, opt)
-        image_tensor = image_tensor.cuda()
-
-
-        y_pred = model(image_tensor)
-        prob = y_pred.sigmoid().flatten().item()
-
+        class_result = "FAKE" if pred>0.5 else "REAL"
+        
         results = {
-            "FakeOrReal": {},
-        }
+            "FakeOrReal" : {'REAL': 1.0 - float(pred), 'FAKE': float(pred), },
+            "DetectionResult": class_result
+        } 
+        
+        #tensor to numpy
+        npr_np = NPR.squeeze(0).numpy().transpose(1,2,0)
+        npr_np = (npr_np * 255).astype(np.uint8)
 
-        results["FakeOrReal"]["REAL"] = 1-prob
-        results["FakeOrReal"]["FAKE"] = prob
-
-        npr_image = self.npr_image(img)
-        npr_image_np= npr_image.numpy().transpose(1, 2, 0)
-
-        # save image as tensor (more accurate image)
-        # save_image(npr_image, '/user/npr_image.png')
+         # save image as tensor (more accurate image)
+        # save_image(NPR, '/user/npr_image.png')
 
         # save image as numpy
-        # cv2.imwrite("/user/npr_image_fake.png", npr_image_np)
+        # cv2.imwrite("/user/npr_image.png", npr_np)
 
-        out_images = [cropped_img_np, npr_image_np]
-        print(results)
+        out_images = [cropped_img_np, npr_np]
 
         return results, out_images
 
